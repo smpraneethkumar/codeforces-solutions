164fe2391603cbb309f9206082728302de3ab6a1
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-02-01 23:37:58 +0530

    domino piling 50A

---
+++ src/questions/easy_A/Domino_piling_50A.java
@@ -0,0 +1,20 @@
+package questions.easy_A;
+
+    import java.util.*;
+public class Domino_piling_50A {
+
+
+
+        public static void main(String[] args){
+
+            Scanner scan = new Scanner(System.in);
+
+            int m = scan.nextInt();
+            int n = scan.nextInt();
+
+            int output = (m *n) / 2;
+
+            System.out.println(output);
+        }
+    }
+
