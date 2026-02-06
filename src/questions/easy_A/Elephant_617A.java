a8a92b5c2a27109827045f45f62a7850e7b9f2ca
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-02-06 22:46:19 +0530

    elephant 617a

---
+++ src/questions/easy_A/Elephant_617A.java
@@ -0,0 +1,11 @@
+package questions.easy_A;
+import  java.util.*;
+public class Elephant_617A {
+
+    public static void main(String[] args) {
+
+        int dis = 12;
+            int ans = (dis +4) /5;
+        System.out.println(ans);
+    }
+}
