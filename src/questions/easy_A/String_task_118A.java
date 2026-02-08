87d41357ab6fe0dfdfe672fd6781b71eeb375212
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-02-08 19:58:53 +0530

    string task 118a

---
+++ src/questions/easy_A/String_task_118A.java
@@ -0,0 +1,30 @@
+package questions.easy_A;
+
+    import java.util.*;
+public class String_task_118A {
+
+
+
+        public static void main(String[] args) {
+
+            Scanner scan = new Scanner(System.in);
+
+            String input = scan.next().toLowerCase();
+
+            int n = input.length();
+
+            String output = "";
+            for (int i = 0; i < n; i++) {
+
+                char c = input.charAt(i);
+//                output += '.';
+                if (c != 'a' && c != 'o' && c != 'y' &&c != 'e' && c != 'u' && c != 'i') {
+
+                    output +="." + c;
+
+                }
+
+            }
+                System.out.println(output);
+        }
+}
