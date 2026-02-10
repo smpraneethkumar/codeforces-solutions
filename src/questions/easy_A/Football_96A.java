0a3b4346abdb37fdbdde753c943f605367b08456
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-02-10 13:47:17 +0530

    football 96a

--- src/questions/easy_A/Football_96A.java
+++ src/questions/easy_A/Football_96A.java
@@ -5,33 +5,25 @@ public class Football_96A {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
 
+        int count = 1;
 
-
-                Scanner scan = new Scanner(System.in);
-
-
-                String input = scan.next();
-                int count = 0;
-
-                for(int i =0;i<input.length();i++){
-
-                    char c = input.charAt(i);
-                    char d = input.charAt(i+1);
-                    if(c =='0' &&  d =='0'){
-                        count++;
-                    }
-                }
-
-                if(count >= 7){
-
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == input.charAt(i - 1)) {
+                count++;
+                if (count == 7) {
                     System.out.println("YES");
-                }else{
-                    System.out.println("NO");
+                    return;
                 }
-
-
-
+            } else {
+                count = 1;
+            }
         }
+
+        System.out.println("NO");
+
+    }
 
 }
