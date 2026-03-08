d64db69f6a0ff3d95165ecb07415a3ee57fb5a28
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-03-08 23:56:11 +0530

    Calculating_Function_486A

---
+++ src/questions/march/Calculating_Function_486A.java
@@ -0,0 +1,17 @@
+package questions.march;
+
+import java.util.Scanner;
+
+public class Calculating_Function_486A {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        if(n % 2 == 0){
+            System.out.println(n/2);
+        }else{
+            System.out.println(-(n+1)/2);
+        }
+    }
+}
