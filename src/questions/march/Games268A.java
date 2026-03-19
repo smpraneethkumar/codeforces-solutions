ddd8919c005492ccc0e0938ef3e298f526884d5a
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-03-19 12:53:08 +0530

    games268A

---
+++ src/questions/march/Games268A.java
@@ -0,0 +1,38 @@
+package questions.march;
+
+import java.util.*;
+
+public class Games268A{
+    
+    public static void main(String[] args){
+        
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int n= scan.nextInt();
+        
+        int[] h = new int[n];
+        int[] a = new int[n];
+        int count = 0;
+        
+        for(int i =0;i<n;i++){
+            
+            h[i] = scan.nextInt();
+            a[i]= scan.nextInt();
+        }
+        
+        for(int i =0;i<n;i++){
+            
+            for(int j =0;j<n;j++){
+                
+                if(i != j && a[i] == h[j]){
+                    
+                    count++;
+                }
+            }
+        }
+        
+        System.out.println(count);
+        
+    }
+}
