78d5f76145fd89a30df4ae524bacb5f25ef65590
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-03-31 22:47:38 +0530

    Soft_drinking_151A

---
+++ src/questions/march/Soft_drinking_151A.java
@@ -0,0 +1,31 @@
+package questions.march;
+
+import java.util.Scanner;
+
+public class Soft_drinking_151A {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+        int l = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+        int p = scan.nextInt();
+        int nl = scan.nextInt();
+        int np = scan.nextInt();
+
+        int drinks =  k * l / n;
+        int a = c*d;
+        int toastsSalt = p/np;
+
+
+        int ans = Math.min(drinks,Math.min(a,toastsSalt));
+
+
+
+
+        System.out.println(ans/n);
+    }
+}
