8500c205dd9a81e0215c42d39f9ea819e29d4605
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-04-15 19:06:43 +0530

    Hq9+ 133A is already done but not updated in codeforces

--- src/questions/march/HQ9_133A.java
+++ src/questions/march/HQ9_133A.java
@@ -1,26 +1,26 @@
-package questions.march;
+    package questions.march;
 
-import java.util.*;
+    import java.util.*;
 
-public class HQ9_133A{
-    
-    public static void main(String[] args){
-        
-        Scanner scan = new Scanner(System.in);
-        
-        String p = scan.next();
-        
-        for(int i =0;i<p.length();i++){
-            
-            char c= p.charAt(i);
-            
-            if(c == 'H' || c == 'Q' || c == '9'){
-                
-                System.out.println("YES");
-                return;
+    public class HQ9_133A{
+
+        public static void main(String[] args){
+
+            Scanner scan = new Scanner(System.in);
+
+            String p = scan.next();
+
+            for(int i =0;i<p.length();i++){
+
+                char c= p.charAt(i);
+
+                if(c == 'H' || c == 'Q' || c == '9'){
+
+                    System.out.println("YES");
+                    return;
+                }
             }
+
+            System.out.println("NO");
         }
-        
-        System.out.println("NO");
     }
-}
